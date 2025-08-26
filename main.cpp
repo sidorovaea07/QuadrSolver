b0727c78de39608408d498ec487a736952ce9144
sidorovaea07 <lenasidorova007@gmail.com>
2025-08-26 23:49:56 +0300

    Исправила опечатку "c" на "s"

--- main.cpp
+++ main.cpp
@@ -5,13 +5,15 @@
 #include "InputOutput.h"
 
 int main () {
+    
     struct cfc s = {NAN, NAN, NAN, NANRoots, NAN, NAN};
     int passed = TestsReader();
     int handpassed = RunHandTest();
     printf("\033[35mHANDPassed %d.\033[0m\n", handpassed);
     printf("\033[35mPassed %d.\033[0m\n", passed);
+
     printf("Enter \"s\" to start\n");
-    while (getchar() == 'c') {
+    while (getchar() == 's') {
         printf("Enter the coefficients of the quadratic equation in the format ax^2 + bx + c = 0.\n");
         Input(&s);
         SquareSolv(&s);
